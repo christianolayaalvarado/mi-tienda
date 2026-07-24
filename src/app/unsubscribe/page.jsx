862ae5e7f9676d8316b2cf5166893b92483c4d2e
chris <christianolayaalvarado@gmail.com>
2026-07-24 12:00:58 -0500

    fix: use next/navigation instead of next/suspense

--- src/app/unsubscribe/page.jsx
+++ src/app/unsubscribe/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useSearchParams } from "next/suspense";
+import { useSearchParams } from "next/navigation";
 import Link from "next/link";
 import { Suspense } from "react";
 
